1185126d204da8b7d74ace3501579de943484084
Shane Vitarana <shanev@gmail.com>
2016-02-14 17:55:09 -0500

    Added Xcode project to build as a framework

---
+++ UIImageViewAlignedSwift/UIImageViewAlignedSwift/UIImageViewAlignedSwift.h
@@ -0,0 +1,19 @@
+//
+//  UIImageViewAlignedSwift.h
+//  UIImageViewAlignedSwift
+//
+//  Created by Shane Vitarana on 2/14/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageViewAlignedSwift.
+FOUNDATION_EXPORT double UIImageViewAlignedSwiftVersionNumber;
+
+//! Project version string for UIImageViewAlignedSwift.
+FOUNDATION_EXPORT const unsigned char UIImageViewAlignedSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageViewAlignedSwift/PublicHeader.h>
+
+
